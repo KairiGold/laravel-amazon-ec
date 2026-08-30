7768f809c9c3a8e517fd748586a2cde69ede4a7c
CaoAnderson <kairi.220284@gmail.com>
2026-08-30 20:57:16 +0900

    演習の提出

--- resources/views/users/cart_history_show.blade.php
+++ resources/views/users/cart_history_show.blade.php
@@ -1,78 +1,108 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container d-flex justify-content-center mt-3">
-    <div class="w-75">
-        <span>
-            <a href="{{ route('mypage') }}">マイページ</a> > <a href="{{ route('mypage.cart_history') }}">注文履歴</a> > 注文詳細
-        </span>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <span>
+                <a href="{{ route('mypage') }}">マイページ</a> > <a href="{{ route('mypage.cart_history') }}">注文履歴</a> > 注文履歴詳細
+            </span>
 
-        <h1 class="mt-3">注文詳細</h1>
+            <h1 class="mt-3">注文履歴詳細</h1>
 
-        <div class="row mt-3">
-            <div class="col-6">
-                <label>注文番号</label>
-                <p>{{ $cart_info->code }}</p>
-            </div>
-            <div class="col-6">
-                <label>購入日時</label>
-                <p>{{ $cart_info->updated_at }}</p>
-            </div>
-        </div>
+            <h4 class="mt-3">ご注文情報</h4>
 
-        <hr>
+            <hr>
 
-        <div class="row">
-            <div class="offset-8 col-4">
-                <div class="row">
-                    <div class="col-6">
-                        <h2>数量</h2>
-                    </div>
-                    <div class="col-6">
-                        <h2>合計</h2>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <hr>
-
-        <div class="row">
-            @foreach ($cart_contents as $product)
-            <div class="col-md-2 mt-2">
-                @if ($product->options->image ?? false)
-                <img src="{{ asset($product->options->image) }}" class="img-fluid w-100">
-                @else
-                <img src="{{ asset('img/DSC_0149.JPG')}}" class="img-fluid w-100">
-                @endif
-            </div>
-            <div class="col-md-6 mt-4">
-                <h3 class="mt-4">{{ $product->name }}</h3>
-            </div>
-            <div class="col-md-2">
-                <h3 class="w-100 mt-4">{{ $product->qty }}</h3>
-            </div>
-            <div class="col-md-2">
-                <h3 class="w-100 mt-4">￥{{ $product->qty * $product->price }}</h3>
-            </div>
-            @endforeach
-        </div>
-
-        <hr>
-
-        <div class="offset-8 col-4">
             <div class="row">
-                <div class="col-6">
-                    <h2>合計</h2>
+                <div class="col-5 mt-2">
+                    注文番号
                 </div>
-                <div class="col-6">
-                    <h2>￥{{ $cart_info->price_total }}</h2>
+                <div class="col-7 mt-2">
+                    {{ $cart_info->code }}
                 </div>
-                <div class="col-12 d-flex justify-content-end">
-                    表示価格は税込みです（送料含む）
+
+                <div class="col-5 mt-2">
+                    注文日時
                 </div>
+                <div class="col-7 mt-2">
+                    {{ $cart_info->updated_at }}
+                </div>
+
+                <div class="col-5 mt-2">
+                    合計金額
+                </div>
+                <div class="col-7 mt-2">
+                    {{ $cart_info->price_total }}円
+                </div>
+
+                <div class="col-5 mt-2">
+                    合計数量
+                </div>
+                <div class="col-7 mt-2">
+                    {{ $cart_info->qty }}
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="row">
+                @foreach ($cart_contents as $product)
+                <div class="col-md-5 mt-2">
+                    <a href="{{route('products.show', $product->id)}}" class="ml-4">
+                        @if ($product->options->image)
+                        <img src="{{ asset($product->options->image) }}" class="img-fluid w-75">
+                        @else
+                        <img src="{{ asset('img/DSC_0149.JPG')}}" class="img-fluid w-75">
+                        @endif
+                    </a>
+                </div>
+                <div class="col-md-7 mt-2">
+                    <div class="flex-column">
+                        <p class="mt-4">{{$product->name}}</p>
+                        <div class="row">
+                            <div class="col-2 mt-2">
+                                数量
+                            </div>
+                            <div class="col-10 mt-2">
+                                {{$product->qty}}
+                            </div>
+
+                            <div class="col-2 mt-2">
+                                小計
+                            </div>
+                            <div class="col-10 mt-2">
+                                ￥{{$product->qty * $product->price}}
+                            </div>
+
+                            <div class="col-2 mt-2">
+                                送料
+                            </div>
+                            <div class="col-10 mt-2">
+                                @if ($product->options->carriage)
+                                ￥800
+                                @else
+                                ￥0
+                                @endif
+                            </div>
+
+                            <div class="col-2 mt-2">
+                                合計
+                            </div>
+                            <div class="col-10 mt-2">
+                                @if ($product->options->carriage)
+                                ￥{{($product->qty * $product->price) + 800}}
+                                @else
+                                ￥{{$product->qty * $product->price}}
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
             </div>
         </div>
     </div>
 </div>
+
 @endsection
